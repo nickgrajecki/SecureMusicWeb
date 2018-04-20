f268040410ad8b85a0315652fe790b6a28bbd620
sag14gzu <sag14gzu@cmpmsc-23.cmp.uea.ac.uk>
2018-04-20 14:47:13 +0100

    Merge origin/master
    Conflicts:
    	web/ReceptionView.html

--- SecureMusicWeb/web/SecureMusicJS.js
+++ SecureMusicWeb/web/SecureMusicJS.js
@@ -15,5 +15,23 @@ function switchToRegister() {
 $(document).ready(function () {
     $("#register").hide();
     $("#login").show();
+    $('#passwordReminder').hide();
 });
 
+function setPassword() {
+    document.getElementById('passwordReminder').style.display = 'block';
+}
+
+function hideSetPassword() {
+    document.getElementById('passwordReminder').style.display = 'none';
+}
+
+//function noUserDisplay() {
+//    if(document.getElementById("user").value = )
+//        return true;
+//    else if (document.getElementById("pass").value == verified)
+//        return true;
+//    else
+//        document.getElementById("error").innerHTML = "Username or Password is incorrect"
+//        return false;
+//}
