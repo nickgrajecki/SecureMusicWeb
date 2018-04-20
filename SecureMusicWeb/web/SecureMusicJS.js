efb527ba8ed154ddb7c6e0d5692cd312d0e08cdb
sag14gzu <sag14gzu@cmpmsc-23.cmp.uea.ac.uk>
2018-04-20 15:09:59 +0100

    Merge origin/master
    Conflicts:
    	web/ReceptionView.html

--- SecureMusicWeb/web/SecureMusicJS.js
+++ SecureMusicWeb/web/SecureMusicJS.js
@@ -1,35 +1,44 @@
+//Brings up login options when clicking 'login'
 function switchToLogin() {
     
     document.getElementById('register').style.display = 'none';
     document.getElementById('login').style.display = 'block';
+    document.getElementById('log').style.cssText = 'background-color: white';
+    document.getElementById('reg').style.cssText = 'background-color: dark-gray';
     
 }
 
+//Brings up register options when clicking 'register'
 function switchToRegister() {
     
     document.getElementById('login').style.display = 'none';
     document.getElementById('register').style.display = 'block';
+    document.getElementById('reg').style.cssText = 'background-color: white';
+    document.getElementById('log').style.cssText = 'background-color: dark-gray';
     
 }
 
+//How the webpage appears upon load in
 $(document).ready(function () {
     $("#register").hide();
     $("#login").show();
     $('#passwordReminder').hide();
 });
 
+//shows password requirements upon clicking 'password' input
 function setPassword() {
     document.getElementById('passwordReminder').style.display = 'block';
 }
 
+//hides password requirements upon clicking elsewhere
 function hideSetPassword() {
     document.getElementById('passwordReminder').style.display = 'none';
 }
 
 //function noUserDisplay() {
-//    if(document.getElementById("user").value = )
+//    if(document.getElementById("user").value == null)
 //        return true;
-//    else if (document.getElementById("pass").value == verified)
+//    else if (document.getElementById("pass").value == null)
 //        return true;
 //    else
 //        document.getElementById("error").innerHTML = "Username or Password is incorrect"
